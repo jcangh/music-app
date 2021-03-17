bb99b94cc8b28a66248086f7833da92993254514
Juan Carlos Angulo <ing.jca@hotmail.com>
2021-03-16 20:40:42 -0500

    warnings fixed

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ var app = require('./app');
 var port = process.env.PORT || 3977;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/music_app', (err,res) => {
+mongoose.connect('mongodb://localhost:27017/music_app', 
+    { useNewUrlParser: true,
+        useUnifiedTopology: true },
+    (err,res) => {
     if (err){
         throw err;
     }else{
